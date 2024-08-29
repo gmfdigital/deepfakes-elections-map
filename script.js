a5587f9eed7d833651a1430323cae2bb3cb71e38
gmfdigital <126702905+gmfdigital@users.noreply.github.com>
2024-08-29 13:34:05 -0400

    Update script.js no more EUUS

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var continentToggleContainer = document.querySelector(".continent-toggle-container");
+/*var continentToggleContainer = document.querySelector(".continent-toggle-container");
 var unitedStatesToggle = document.querySelector("#united-states");
 var europeToggle = document.querySelector("#europe");
 var USAbbox = [[-125, 51], [-68,20 ]];
-var EUbbox = [[-12, 61], [44,30 ]];
+var EUbbox = [[-12, 61], [44,30 ]];*/
 var legend = document.querySelector(".legend"); 
 var categoryContainer = document.querySelector(".category-container"); 
 
 map.changingContinent = false;
 
 
-unitedStatesToggle.addEventListener("click", function(){
+/*unitedStatesToggle.addEventListener("click", function(){
 	toggleClass(unitedStatesToggle, europeToggle, "clicked");
 	fullPopup('clear', 'clear');
 	map.once('movestart', () => { map.changingContinent = true; });
@@ -46,7 +46,7 @@ function checkContinent() {
 		toggleClass(unitedStatesToggle, europeToggle, "active");
 		toggleClass(unitedStatesToggle, europeToggle, "clicked")	 
 	} 
-}
+}*/
 
 
 function peekDirection() {
