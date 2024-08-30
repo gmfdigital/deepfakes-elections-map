fe1d7936d98d75f12ee2bc15e66c51bee5266289
gmfdigital <126702905+gmfdigital@users.noreply.github.com>
2024-08-30 12:03:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var continentToggleContainer = document.querySelector(".continent-toggle-container");
-//var unitedStatesToggle = document.querySelector("#united-states");
-//var europeToggle = document.querySelector("#europe");
+var unitedStatesToggle = document.querySelector("#united-states");
+var europeToggle = document.querySelector("#europe");
 //var USAbbox = [[-125, 51], [-68,20 ]];
 //var EUbbox = [[-12, 61], [44,30 ]];
 var legend = document.querySelector(".legend");
